e71c2541315a605c41a82d950b2a0b94b5e52d80
Samantha Le Huec <s.lhuec@gmail.com>
2022-11-22 17:00:36 +0100

    init ex04, sans partir en cacahuete

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sam <sam@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 13:09:48 by sam               #+#    #+#             */
-/*   Updated: 2022/11/22 16:00:00 by sam              ###   ########.fr       */
+/*   Updated: 2022/11/22 16:59:03 by sam              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,6 @@
 #include "colors.h"
 
 
-// Ouvrir un fichier en écriture (écire dedans)
-
-// on déclare un flux exactement de la même manière qu'une variable, 
-// une variable dont le type serait ofstream  , et dont la valeur serait 
-// le chemin d'accès du fichier à lire.
- 
-// Si le fichier n'existait pas, le programme le créerait automatiquement ! 
-// Par contre, il faut que le dossier existe. Dans l'exemple
-// précédent, le dossier C:/Nanoc/C++/Fichiers doit exister. Si ce n'est pas le cas, rien ne sera écrit.
-
-
-// Ouvrir un fichier en lecture
-
-
-//Les fichiers ouverts sont automatiquement refermés lorsque l'on sort du bloc où le flux est déclaré.
 
 int main(int ac, char **av)
 {
@@ -43,5 +28,7 @@ int main(int ac, char **av)
 		return -1;
 	}
 	else
-		std::cout << "pouet" << std::endl;
+	{
+		
+	}
 }
