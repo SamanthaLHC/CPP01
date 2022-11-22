2c8a9ed32ea81ad9605416e320767f10f20dc222
Samantha Le Huec <s.lhuec@gmail.com>
2022-11-22 17:42:54 +0100

    save ex04 en cour

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sam <sam@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 13:09:48 by sam               #+#    #+#             */
-/*   Updated: 2022/11/22 16:59:03 by sam              ###   ########.fr       */
+/*   Updated: 2022/11/22 17:33:34 by sam              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,26 @@
 
 #include "colors.h"
 
+int read_file(std::string param)
+{
+	std::ifstream	file_in_param(param);
+	if(file_in_param.fail())
+	{
+		std::cout << BRED << "ERROR: invalid file." << RES << std::endl;
+		return -1;
+	}
+	else
+	{
+		std::string line = "";
+		std::ofstream 
+		while (std::getline(file_in_param, line))
+		{
 
+		}
+	}
+	
+
+}
 
 int main(int ac, char **av)
 {
@@ -29,6 +48,9 @@ int main(int ac, char **av)
 	}
 	else
 	{
-		
+		if (read_file(av[1]) < 0)
+			return -1;
+
+
 	}
 }
